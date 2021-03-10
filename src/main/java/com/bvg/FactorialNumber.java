c22f460d14cfc19cce9a78b76e367c497e6e3575
B V Gajjar <bvg@bs-macbook-pro.lan>
2021-03-10 11:44:22 -0600

    FactorialNumber

--- src/main/java/com/bvg/FactorialNumber.java
+++ src/main/java/com/bvg/FactorialNumber.java
@@ -1,6 +1,6 @@
 package com.bvg;
 
-public class FractionNumber {
+public class FactorialNumber {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- src/main/java/com/bvg/PrimeNumber.java
+++ src/main/java/com/bvg/PrimeNumber.java
@@ -1,34 +1,28 @@
 package com.bvg;
 
 public class PrimeNumber {
-   
-		 public static void main(String args[]){    
-			 
-		  int i,m=0,flag=0;      
-		  
-		  int n=17;  
-		  
-		  m=n/2;      
-		  
-		  if(n==0||n==1){  
-			  
-		   System.out.println(n+" is not prime number"); 
-		   
-		  }
-		  else{ 
-			  
-		   for(i=2;i<=m;i++){   
-			   
-		    if(n%i==0){      
-		    	
-		     System.out.println(n+" is not prime number");   
-		     
-		     flag=1; 
-		     
-		     break;      
-		    }      
-		   }      
-		   if(flag==0)  { System.out.println(n+" is prime number"); }  
-		  } 
-		}    
-		}   
+
+	public static void main(String args[]) {
+
+		int m = 0, flag = 0;
+		int value= 13;
+		m = value / 2;
+
+		if (value == 0 || value == 1) {
+			System.out.println(value + " is not prime number");
+
+		} 
+		else {
+			for (int i = 2; i <= m; i++) {
+				if (value % i == 0) {
+					System.out.println(value + " is not prime number");
+					flag = 1;
+					break;
+				}
+			}
+			if (flag == 0) {
+				System.out.println(value + " is prime number");
+			}
+		}
+	}
+}
