9ef428139d005cc057265d33cbd63ccf4cfa56f1
B V Gajjar <bvg@bs-macbook-pro.lan>
2021-03-10 11:18:58 -0600

    factorial number

---
+++ src/main/java/com/bvg/FractionNumber.java
@@ -0,0 +1,18 @@
+package com.bvg;
+
+public class FractionNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num = 10;
+
+		long factorial = 1;
+
+		for (int i = 1; i <= num; ++i) {
+			// factorial = factorial * i;
+			factorial *= i;
+		}
+		System.out.printf("Factorial of %d = %d", num, factorial);
+	}
+}
