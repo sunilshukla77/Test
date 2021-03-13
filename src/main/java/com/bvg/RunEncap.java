f7014845c80dd29bdd77671650555ac10bee41ed
B V Gajjar <bvg@Bs-MacBook-Pro.local>
2021-03-12 19:10:09 -0600

    encap

---
+++ src/main/java/com/bvg/RunEncap.java
@@ -0,0 +1,16 @@
+package com.bvg;
+
+public class RunEncap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		EncapTest encap = new EncapTest();
+	      encap.setName("James");
+	      encap.setAge(20);
+	      encap.setIdNum("12343ms");
+	 
+	System.out.print("Name : "+ encap.getName()+  " Age : "+ encap.getAge()+ " IdNum : "+ encap.getIdNum());
+	
+	}
+	}
