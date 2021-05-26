15d8fe032a061c91c7a0e0b983d0ad16a23ce901
sunil <sunil.jobs77@gmail.com>
2021-05-26 22:22:37 +0530

    stream

--- src/main/java/com/aspire/StreamEx3.java
+++ src/main/java/com/aspire/StreamEx3.java
@@ -1,43 +1,69 @@
 package com.aspire;
 
-import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamEx3 {
-	private static Employee[] arrayOfEmps = {
-			new Employee(1,"Jeff Bezos", 100000.0),
-			new Employee(2,"Bill Gates", 200000.0),
-			new Employee(3,"Mark", 300000.0)
-					};
-	Stream<Employee> employees = Stream.of(arrayOfEmps);
-	public static Employee[] getArrayOfEmps() {
-		return arrayOfEmps;
+
+	public static void main(String[] args) {
+	
+		List<Employee> le= new ArrayList();
+		le.add(new Employee(1,"Sum", 1000d));
+		le.add(new Employee(2,"Sun", 2000d));
+		le.add(new Employee(3,"Arr", 3000d));
+		le.add(new Employee(4,"Sud", 16000d));
+		le.add(new Employee(5,"Sup", 8000d));
+		le.add(new Employee(6,"Bat", 5000d));
+		
+		le.stream().filter(e-> e.getSalary()>5000d).collect(Collectors.toList()).forEach(System.out::println);
+		
 	}
 
-	public static void setArrayOfEmps(Employee[] arrayOfEmps) {
-		StreamEx3.arrayOfEmps = arrayOfEmps;
-	}
-	}
-class Employee{
+}
+
+class Employee {
+	
 	private Integer id;
 	private String name;
 	private Double salary;
 	
-	@Override
-	public String toString() {
-		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
-	}
-	public Object getId() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	public Employee (Integer id, String name, Double salary) {
+	public Employee(Integer id, String name, Double salary) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.salary = salary;
-		
-		}
+
+	}
+	
+	public Integer getId() {
+		return id;
 	}
 
-		
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+	}
+
+	
+}
